94ca5be428f31567cd1c5a70a1fccf40088aea44
alice-odamtten <119321981+alice-odamtten@users.noreply.github.com>
2023-05-16 09:22:11 +0000

    Mon, May 15, 2023, 9:22 PM -12:00

--- _puts.c
+++ _puts.c
@@ -15,3 +15,19 @@ void _puts(const char *s)
 }
 
 
+/**
+ * _prerror - Prints an error message indicating that a command is not found
+ * @e: The name of the command that was not found
+ *
+ * Return: void
+ */
+
+void _prerror(char *e)
+{
+        char *y;
+
+        y = malloc(sizeof(char *) * _strlen(e) + _strlen(" :not found"));
+        y = _strcat(e, ":not found");
+        write(STDOUT_FILENO, y, _strlen(y));
+        write(STDOUT_FILENO, "\n", 1);
+}

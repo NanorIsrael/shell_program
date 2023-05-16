27baa8dc30f4e1fe2acc961089da6d8cb6e6dcd6
alice-odamtten <119321981+alice-odamtten@users.noreply.github.com>
2023-05-16 09:15:35 +0000

    Mon, May 15, 2023, 9:15 PM -12:00

---
+++ _puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** _puts - print strings with new line
+ * @s: the string to print
+ */
+
+void _puts(const char *s)
+{
+        while (*s != '\0')
+        {
+                _putchar(*s);
+                s++;
+        }
+        _putchar('\n');
+}
